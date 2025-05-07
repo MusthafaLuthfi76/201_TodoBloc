29118a3175295a457d1ca3da786844eb3e65fffe
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-05-07 22:12:33 +0700

    Menambahkan tombol untuk memilih tanggal pada TodoPage

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -33,6 +33,25 @@ class TodoPage extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        ).then((selectedDate) {
+                          if (selectedDate != null) {
+                            context.read<TodoBloc>().add(
+                              TodoSelectDate(date: selectedDate),
+                            );
+                          }
+                        });
+                      },
+                      child: Text("Select Date"),
+                    ),
+                  ),
                 ],
               ),
             ],
